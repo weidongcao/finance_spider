158fd9a69f979aeda8736af7f6aca458243d8181
rainsoft <1774104802@qq.com>
2018-11-19 14:19:43 +0800

    Properties工具类优化

--- src/main/com/dong/utils/PropertiesUtil.py
+++ src/main/com/dong/utils/PropertiesUtil.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import os
 
 
 class PropertiesUtil(object):
@@ -11,13 +12,15 @@ class PropertiesUtil(object):
         :param file_path: 文件路径
         :return:字典内容的key、value均为字符串
         """
-        with open(file_path, 'r', encoding='UTF-8') as pro_file:
+        conf_dir = os.path.dirname(os.path.abspath(__file__))
+        conf_file = os.path.join(conf_dir, file_path)
+        with open(conf_file, 'r', encoding='utf8') as pro_file:
             for line in pro_file.readlines():
                 line = line.strip().replace('\n', '')
                 if line.find('=') > 0 and not line.startswith('#'):
                     strs = line.split('=')
                     value = line[len(strs[0]) + 1:]
-                    self.__conf__[strs[0].strip(), value.strip()]
+                    self.__conf__[strs[0].strip()] = value.strip()
 
     def get_value(self, key):
         """
